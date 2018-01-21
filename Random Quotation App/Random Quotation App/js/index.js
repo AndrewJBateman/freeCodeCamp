d0ddf928c5b94cf996da7a77e89db4b698570507
Andrew Bateman <gomezbateman@yahoo.com>
2018-01-21 19:39:46 +0100

    Button layout corrrected, commented Javascript, removed surplus code
    Quote and twitter buttons now adjacent in all screen sizes. Commented javascript using jQuery library methods. Small improvements to look of page (Quote button opacity used, quote box size reduced.)

--- Random Quotation App/Random Quotation App/js/index.js
+++ Random Quotation App/Random Quotation App/js/index.js
@@ -1,4 +1,4 @@
-/*This function gets just 1 JSON-formatted quote from an API weblink then displays only the actual quote and the author from the API results */
+/*Uses jQuery libary methods ($) to get JSON-formatted text from an API weblink then displays only quote (as #quote) & author (as #author) from the results in html format.  */
 function getQuote () {
  $.getJSON( "https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1",
       function(apiResult) {
@@ -6,13 +6,14 @@ function getQuote () {
         $('#author').html("- " + apiResult[0].title);
       });
 }
+/*jQuery library methods to run a function to get a new quote when the #quote-button is pressed. */
 $(document).ready(function() {
   getQuote();
-  $.ajaxSetup({cache: false});
+  $.ajaxSetup({cache: false}); /*sets default value for future ajax requests. Cache indicates if browser should cache page (boolean: default True) */
   $("#quote-button").on("click", function() {
     getQuote();
   });
-  
+  /*When #twitter-button pressed, opwn window at address shown and display #quote and #author*/
   $("#twitter-button").on("click", function(){
   window.open("https://twitter.com/intent/tweet?hashtags=quote&text=" + 
               '"' + 
